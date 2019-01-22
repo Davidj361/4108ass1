95a2a56907b03eb9fb6e7b7cc1c24ac0a90dad56
Davidj361 <david.j.361@gmail.com>
2019-01-22 13:05:33 -0500

    added md5 cmd

--- part2.cc
+++ part2.cc
@@ -17,6 +17,7 @@
 // 2nd argument is the file to crack
 // 3rd argument is output
 int main(int argc, char *argv[]) {
+  // TODO: check for substring "bad decrypt" from openssl if it's a bad pass
   if (argc < 2) {
     std::cout << "Need the input file to crack" << std::endl;
     return -1;
@@ -25,8 +26,8 @@ int main(int argc, char *argv[]) {
   std::string inFileBase = basename((char*)inFile.c_str());
   std::string decodedFile = DIRECTORY + inFileBase + ".decoded";
   std::string decryptedFile = DIRECTORY + inFileBase + ".decrypted";
-  char* line = NULL;
-  size_t lineSize = 0;
+  std::string passFile = DIRECTORY + inFileBase + ".hash";
+  std::string pass = "lolsecret";
   int fd[2];
   if (pipe(fd) == -1) {
     std::cout << "Bad pipe creation" << std::endl;
@@ -35,6 +36,9 @@ int main(int argc, char *argv[]) {
 
   int fdDecodedFile = open(decodedFile.c_str(), O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
   std::string base64CMD = "base64 -d " + inFile + " > " + decodedFile; // e.g. base64 -d practice_file.aes256. > /tmp/in2
+  std::string md5CMD = "echo -n " + pass + " | md5sum > " + passFile;
+  // openssl enc -d -aes256 -in /tmp/practice_file.aes256.nohash.txt.decoded -out /tmp/1 -pass pass:lolsecret
+  std::string opensslCMD = "openssl enc -d -aes256 -in " + decodedFile + " -out " + decryptedFile + " -pass pass:" + pass;
 
   std::cout << "writing to: " + decodedFile << std::endl;
   FILE* fBase64 = popen(base64CMD.c_str(), "w");
@@ -44,6 +48,23 @@ int main(int argc, char *argv[]) {
   }
   pclose(fBase64);
 
+  std::cout << "writing to: " + passFile << std::endl;
+  FILE* fMd5 = popen(md5CMD.c_str(), "w");
+  if (fMd5 == NULL) {
+    std::cout << "fBase64 is a null pointer" << std::endl;
+    exit(1);
+  }
+  pclose(fMd5);
+
+  std::cout << "writing to: " + decryptedFile << std::endl;
+  FILE* fOpenssl = popen(opensslCMD.c_str(), "w");
+  if (fBase64 == NULL) {
+    std::cout << "fOpenssl is a null pointer" << std::endl;
+    exit(1);
+  }
+  pclose(fOpenssl);
+
+  /*
   int pid = fork();
 
   if (pid < 0) {
