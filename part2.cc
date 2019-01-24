08e4ad60f4dddc864d4e36421c5f434d3dec7340
Davidj361 <david.j.361@gmail.com>
2019-01-23 23:00:12 -0500

    working part B cracker

--- part2.cc
+++ part2.cc
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 #include <algorithm>
 // C stuff
 #include <fcntl.h>
@@ -20,6 +21,37 @@ bool isFileExist(const char* path) {
   return infile.good();
 }
 
+int decrypt(std::string inFile, std::string decryptedFile, std::string hash) {
+  std::string opensslCMD = "openssl aes256 -base64 -d -in " + inFile + " -out " + decryptedFile + " -k " + hash;
+
+  std::cout << "writing to: " + decryptedFile << std::endl;
+  FILE* fOpenssl = popen(opensslCMD.c_str(), "w");
+  if (fOpenssl == NULL) {
+    std::cout << "fOpenssl is a null pointer" << std::endl;
+    exit(1);
+  }
+  return pclose(fOpenssl);
+}
+
+bool isDecrypted(std::string file) {
+  std::string cmd = "file " + file;
+  FILE* fFile = popen(cmd.c_str(), "r");
+  bool ret = false;
+  if (fFile == NULL) {
+    std::cout << "fFile is a null pointer" << std::endl;
+    exit(1);
+  }
+  char* buf = 0;
+  size_t bufSize;
+  while ( getline(&buf, &bufSize, fFile) != -1) {
+    std::string output = buf;
+    if (output.find("UTF-8") != std::string::npos)
+      ret = true;
+  }
+  pclose(fFile);
+  return ret;
+}
+
 // 1st argument is the program name
 // 2nd argument is the file to crack
 // 3rd argument is output
@@ -34,7 +66,6 @@ int main(int argc, char *argv[]) {
   std::string john = argv[2];
   std::string wordlist = argv[3];
   std::string inFileBase = basename((char*)inFile.c_str());
-  std::string decodedFile = DIRECTORY + inFileBase + ".decoded";
   std::string decryptedFile = DIRECTORY + inFileBase + ".decrypted";
   std::string passFile = DIRECTORY + inFileBase + ".hash";
   std::string checkPass = DIRECTORY + inFileBase + ".check";
@@ -42,18 +73,13 @@ int main(int argc, char *argv[]) {
   size_t bufSize;
   char* passbuf = 0;
   size_t passbufSize;
+  // apparently openssl will think another hash is a successful
+  // decrypt but in actuality it is not
+  // So a count is needed to keep all "decrypted" files where the user
+  // manually checks the files to see if they look decrypted
+  int count = 0;
 
-  std::string base64CMD = "base64 -d " + inFile + " > " + decodedFile; // e.g. base64 -d practice_file.aes256. > /tmp/in2
   std::string johnCMD = john + " --wordlist=" + wordlist + " --rules=single --stdout";
-
-  std::cout << "writing to: " + decodedFile << std::endl;
-  FILE* fBase64 = popen(base64CMD.c_str(), "w");
-  if (fBase64 == NULL) {
-    std::cout << "fBase64 is a null pointer" << std::endl;
-    exit(1);
-  }
-  pclose(fBase64);
-
   FILE* fJohn = popen(johnCMD.c_str(), "r");
   while ( getline(&passbuf, &passbufSize, fJohn) != -1) {
     std::string pass = passbuf;
@@ -77,34 +103,14 @@ int main(int argc, char *argv[]) {
     }
     std::string hash = buf;
     hash.erase(std::remove(hash.begin(), hash.end(), '\n'), hash.end());
-    // openssl enc -d -aes256 -in /tmp/practice_file.aes256.nohash.txt.decoded -out /tmp/1 -pass pass:lolsecret
-    std::string opensslCMD = "openssl enc -d -aes256 -in " + decodedFile + " -out " + decryptedFile + " -pass pass:" + hash + " > '" + checkPass + "' 2>&1";
+    hash.erase(std::remove(hash.begin(), hash.end(), ' '), hash.end());
     fclose(fPassFile);
 
-    std::cout << "writing to: " + decryptedFile << std::endl;
-    FILE* fOpenssl = popen(opensslCMD.c_str(), "w");
-    if (fBase64 == NULL) {
-      std::cout << "fOpenssl is a null pointer" << std::endl;
-      exit(1);
-    }
-    pclose(fOpenssl);
+    decrypt(inFile, decryptedFile, hash);
+    if (!isDecrypted(decryptedFile))
+      continue;
 
-    // Bad ways to check bad password: getline from the command or seeing if decrypt file exists. Both ways failed
-    // "'bad decrypt' should be the first line"
-    if (pass == "access")
-      raise(SIGINT);
-    FILE* fOpensslcheck = fopen(checkPass.c_str(), "r");
-    if ( getline(&buf, &bufSize, fOpensslcheck) == -1) {
-      std::cout << "bad fOpensslcheck read" << std::endl;
-      exit(1);
-    }
-    std::string check = buf;
-    fclose(fOpensslcheck);
-    std::cout << check << std::endl;
-
-    if (check.find("bad decrypt") == std::string::npos) { 
-      std::cout << "Found the password: " << pass << std::endl;
-      break;
-    }
+    std::cout << "Decrypted and found the password: " << pass << std::endl;
+    break;
   }
 }
